20dece5b1b96f327540db287cafca6b361cad2d9
Ahmed-Maalawi <ahmedelmaalawi@gmail.com>
2024-04-23 22:31:29 +0200

    add Survey crud for backend

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,5 @@ Route::post('/login', [AuthController::class,'login'])->name('auth.login');
 
 Route::middleware('auth:sanctum')->group(function () {
    Route::post('/logout', [AuthController::class,'logout'])->name('auth.logout');
+   Route::resource('/survey', SurveyController::class);
 });
